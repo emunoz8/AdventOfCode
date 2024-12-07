c5c07981abbdaf84925d9a2c6326dbc7a742955c
Edwin <emunoz9@neiu.edu>
2024-12-07 16:44:54 -0600

    Day Two, part 1 Working

--- dayTwo/RedNose.java
+++ dayTwo/RedNose.java
@@ -3,50 +3,69 @@ import java.util.ArrayList;
 public class RedNose {
     public static void main(String[] args) {
         ArrayList<int[]> nums = new ArrayList<>();
+        int goodReports = 0;
 
         nums = ReadReports.read("./dayTwo/input.txt");
 
-        System.out.println("Have reports");
+        goodReports = iterateThroughReports(nums);
+
+        System.out.println("The amount of Good Reports is: " + goodReports);
 
     }
 
-    public static int readAndStoreReports() {
+    public static int iterateThroughReports(ArrayList<int[]> nums) {
+        int count = 0;
+        for (int[] num : nums) {
+            if (isGoodReport(num))
+                count++;
+        }
 
-        return -1;
+        return count;
+    }
+
+    public static boolean isGoodReport(int[] nums) {
+        int[] trend = new int[nums.length - 1];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (!isWithingRange(nums, trend, i - 1, i))
+                return false;
+        }
+
+        return true;
     }
 
     public static boolean isWithingRange(int[] nums, int[] trend, int left, int right) {
 
         int diff = nums[left] - nums[right];
 
-        diff = calculateTrend(trend, diff, right);
+        diff = calculateTrend(trend, diff, left);
 
-        if (right > 1 && !checkTrend(trend, right))
+        if (right > 1 && !checkTrend(trend, left - 1, left))
             return false;
 
-        if (1 < diff || diff > 3)
-            return false;
-
-        return true;
-    }
-
-    public static boolean checkTrend(int[] trend, int right) {
-
-        if (trend[right - 1] != trend[right])
+        if (1 > diff || diff > 3)
             return false;
 
         return true;
     }
 
-    public static int calculateTrend(int[] trend, int diff, int right) {
+    public static boolean checkTrend(int[] trend, int left, int right) {
+
+        if (trend[left] != trend[right])
+            return false;
+
+        return true;
+    }
+
+    public static int calculateTrend(int[] trend, int diff, int left) {
 
         if (diff == 0)
-            trend[right] = 0;
+            trend[left] = 0;
         else if (diff < 0) {
             diff = -diff;
-            trend[right] = -1;
+            trend[left] = -1;
         } else {
-            trend[right] = 1;
+            trend[left] = 1;
         }
 
         return diff;
