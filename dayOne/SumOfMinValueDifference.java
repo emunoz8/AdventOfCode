415e452c4b60f5465a58f122f84311334cd5de35
Edwin <emunoz9@neiu.edu>
2024-12-09 22:20:24 -0600

    Finished dayOne, need to test

--- dayOne/SumOfMinValueDifference.java
+++ dayOne/SumOfMinValueDifference.java
@@ -2,73 +2,100 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.BitSet;
+import java.util.ArrayList;
 
 public class SumOfMinValueDifference {
     public static void main(String[] args) {
-        // String url = "https://adventofcode.com/2024/day/1/input";
-        String filePath = "./input.txt";
         int min = 10000, max = 99999, sum = 0;
         int arraySize = max - min + 1;
+        ArrayList<int[]> nums = new ArrayList<>();
+        nums = ReadReports.read("./dayOne/input.txt");
+        sum = iterateThroughLine(nums, arraySize, min);
+        System.out.println("After Part 2: " + sum);
+
+    }
+
+    public static int iterateThroughLine(ArrayList<int[]> nums, int arraySize, int min) {
         BitSet listA = new BitSet(arraySize);
         BitSet listB = new BitSet(arraySize);
         int[] occurancesA = new int[arraySize];
         int[] occurancesB = new int[arraySize];
+        int left, right, sum = 0;
 
-        // WebSaver.createTxt(url, filePath);//Had to login to get the input, maybe next
-        // time
-        readAndStore(filePath, listA, listB, occurancesA, occurancesB, min);
-
-        sum = sumOfDifferences(listA, listB, occurancesA, occurancesB);
-
-        System.out.println("Sum of differences: " + sum);
-
-    }
-
-    public static void readAndStore(String filePath, BitSet listA, BitSet listB, int[] occA, int[] occB, int min) {
-        try {
-            Files.lines(Paths.get(filePath)).forEach(line -> {
-                String[] parts = line.split("\\s+", 2);
-                if (parts.length == 2) {
-                    int i = Integer.parseInt(parts[0]) - min;
-                    int j = Integer.parseInt(parts[1]) - min;
-                    listA.set(i);
-                    listB.set(j);
-                    occA[i]++;
-                    occB[j]++;
-
-                }
-            });
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int[] num : nums) {
+            left = num[0] - min;
+            right = num[1] - min;
+            listA.set(left);
+            listB.set(right);
+            occurancesA[left]++;
+            occurancesB[right]++;
         }
+        sum = sumOfDifferences(listA, listB, occurancesA, occurancesB);
+        System.out.println("from sum of difference method: " + sum);
+        return sum + similarityScore(listA, listB, occurancesA, occurancesB, min);
 
     }
 
-    // sum of differences of min values
+    public static int similarityScore(BitSet listA, BitSet listB, int[] A, int[] B, int min) {
+        int sum = 0, i = 0;
+        while (i != -1) {
+            i = listA.nextSetBit(i + 1);
+            if (i == -1)
+                continue;
+            while (A[i]-- > 0)
+                sum += (i + min) * B[i];
+        }
+        return sum;
+    }
+
     public static int sumOfDifferences(BitSet listA, BitSet listB, int[] A, int[] B) {
         int sum = 0, total = 0;
         int i = 0, j = 0;
+        int[] occA = deepCopy(A);
+        int[] occB = deepCopy(B);
 
         while (i != -1 && j != -1) {
             i = listA.nextSetBit(i + 1);
             j = listB.nextSetBit(j + 1);
-
             sum = i - j;// lists have to be same size; last iteration is -1 - -1 = 0
-
             if (sum < 0)
                 sum = -sum;
-
             total += sum;
-
-            if (i > 0 && --A[i] > 0)
+            if (i > 0 && --occA[i] > 0)
                 i--;
-            if (j > 0 && --B[j] > 0)
+            if (j > 0 && --occB[j] > 0)
                 j--;
-
         }
-
         return total;
     }
 
+    public static int[] deepCopy(int[] array) {
+        int[] rArr = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            rArr[i] = array[i];
+        }
+        return rArr;
+    }
 }
+
+// public static void readAndStore(String filePath, BitSet listA, BitSet listB,
+// int[] occA, int[] occB, int min) {
+// try {
+// Files.lines(Paths.get(filePath)).forEach(line -> {
+// String[] parts = line.split("\\s+", 2);
+// if (parts.length == 2) {
+// int i = Integer.parseInt(parts[0]) - min;
+// int j = Integer.parseInt(parts[1]) - min;
+// listA.set(i);
+// listB.set(j);
+// occA[i]++;
+// occB[j]++;
+
+// }
+// });
+
+// } catch (IOException e) {
+// e.printStackTrace();
+// }
+
+// }
