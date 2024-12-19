67ac9aea24142988bb72bcc08727ad5104a1aaef
Edwin <emunoz9@neiu.edu>
2024-12-19 12:19:20 -0600

    06day part 2

--- 06day/GuardGavillant.java
+++ 06day/GuardGavillant.java
@@ -1,33 +1,86 @@
+
+import java.util.ArrayList;
+
 public class GuardGavillant {
+    final static int[][] DIRECTION = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
     public static void main(String[] args) {
-        final int[][] DIRECTION = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-
         int[] indexOfGuardian;
         char[][] map = ReadReports.set2DArray("./06day/input.txt");
+        ArrayList<int[]> visited = new ArrayList<>();
         int steps = 0;
+        int obstacles = 0;
         indexOfGuardian = findCharacter(map);
 
         if (indexOfGuardian[0] == -1) {
             System.out.println("No Guardian found");
         } else {
-            steps = getAmmountOfPositions(map, DIRECTION, indexOfGuardian[0], indexOfGuardian[1]);
+            steps = getAmmountOfPositions(visited, map, indexOfGuardian[0], indexOfGuardian[1]);
         }
-        printMap(map);
 
         System.out.println(steps);
-        // System.out.println(
-        // indexOfGuardian[0] + " " + indexOfGuardian[1] + " " +
-        // map[indexOfGuardian[0]][indexOfGuardian[1]]);
+
+        for (int[] v : visited) {
+            if (stuckInLoop(map, indexOfGuardian, v[0], v[1], v[2], v[3], v[4]))
+                obstacles++;
+        }
+
+        System.out.print(obstacles);
 
     }
 
-    public static int getAmmountOfPositions(char[][] map, int[][] DIRECTION, int x, int y) {
+    public static boolean stuckInLoop(char[][] map, int[] guardian, int x, int y, int dx, int dy,
+            int turns) {
+        if (guardian[0] == x && guardian[1] == y)
+            return false;
+
+        ArrayList<int[]> visited = new ArrayList<>();
+
+        char holder = map[x][y];
+        int[] object = placeObject(map, x, y, '#');
+
+        x = guardian[0];
+        y = guardian[1];
+        dx = -1;
+        dy = 0;
+        turns = 0;
+        visited.add(new int[] { x, y, dx, dy, turns });
+
+        while (isInbounds(map, x, y, dx, dy)) {
+
+            if (map[x + dx][y + dy] == '#') {
+                turns++;
+            } else {
+                x += dx;
+                y += dy;
+            }
+
+            dx = DIRECTION[turns % 4][0];
+            dy = DIRECTION[turns % 4][1];
+
+            int[] current = new int[] { x, y, dx, dy, turns };
+            if (!hasBeenVisited(visited, current, 4))
+                visited.add(current);
+            else {
+                object = placeObject(map, object[0], object[1], holder);
+                return true;
+            }
+        }
+
+        object = placeObject(map, object[0], object[1], holder);
+        return false;
+    }
+
+    public static int[] placeObject(char[][] map, int x, int y, char character) {
+        map[x][y] = character;
+        return new int[] { x, y };
+    }
+
+    public static int getAmmountOfPositions(ArrayList<int[]> visited, char[][] map, int x, int y) {
         int turns = 0;
         int dx = DIRECTION[turns % 4][0];
         int dy = DIRECTION[turns % 4][1];
-        int count = 1;
-        map[x][y] = 'X';
+        visited.add(new int[] { x, y, dx, dy, turns });
 
         while (isInbounds(map, x, y, dx, dy)) {
             if (map[x + dx][y + dy] == '#') {
@@ -35,17 +88,33 @@ public class GuardGavillant {
             } else {
                 x += dx;
                 y += dy;
-                if (map[x][y] != 'X') {
-                    count++;
-                    map[x][y] = 'X';
-                }
             }
             dx = DIRECTION[turns % 4][0];
             dy = DIRECTION[turns % 4][1];
 
+            int[] current = new int[] { x, y, dx, dy, turns };
+            if (!hasBeenVisited(visited, current, 2))
+                visited.add(current);
         }
 
-        return count;
+        return visited.size();
+    }
+
+    public static boolean hasBeenVisited(ArrayList<int[]> list, int[] check, int maxIndex) {
+
+        for (int[] array : list) {
+            int matches = 0;
+            for (int i = 0; i < maxIndex; i++) {
+                if (array[i] != check[i])
+                    break;
+                else
+                    matches++;
+            }
+            if (matches == maxIndex)
+                return true;
+        }
+
+        return false;
     }
 
     public static boolean isInbounds(char[][] puzzle, int startX, int startY, int dx, int dy) {
@@ -60,6 +129,16 @@ public class GuardGavillant {
             System.out.println();
         }
 
+    }
+
+    public static char[][] deepCopy(char[][] array) {
+        char[][] rArr = new char[array.length][array[0].length];
+
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array[0].length; j++)
+                rArr[i][j] = array[i][j];
+
+        return rArr;
     }
 
     public static int[] findCharacter(char[][] map) {
