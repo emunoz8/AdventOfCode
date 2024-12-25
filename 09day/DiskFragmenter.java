7055ba9c168a9e9c86e5fc29732a4fa41506005d
Edwin <emunoz9@neiu.edu>
2024-12-25 00:13:14 -0600

    Working concept for day 9

--- 09day/DiskFragmenter.java
+++ 09day/DiskFragmenter.java
@@ -1,4 +1,14 @@
 
 public class DiskFragmenter {
 
+    public static void main(String[] args) {
+        int[] amountOfFiles = { 0 };
+        String memory = ReadReports.streamNumbers("./09day/input.txt", amountOfFiles);
+
+        System.out.println(memory);
+
+        System.out.println(amountOfFiles[0]);
+
+    }
+
 }
